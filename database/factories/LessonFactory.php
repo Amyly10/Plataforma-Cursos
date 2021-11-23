1a8763d6cc758d0e397427767923ba37ca716334
Eduardo Chvez Dorado <eduardo.cdorado@alumnos.udg.mx>
2021-11-22 19:33:30 -0600

    LTE install

---
+++ database/factories/LessonFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Lesson;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class LessonFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->sentence(),
+            'url' => 'https://youtu.be/PGQxIILBb7M',
+            'iframe' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/PGQxIILBb7M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+            'platform_id' => 1
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,12 +14,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
+        Storage::deleteDirectory('cursos');
         Storage::makeDirectory('cursos');
 
         $this->call(UserSeeder::class);
         $this->call(LevelSeeder::class);
         $this->call(CategorySeeder::class);
         $this->call(PriceSeeder::class);
+        $this->call(PlatformSeeder::class);
         $this->call(CourseSeeder::class);
     }
 }
